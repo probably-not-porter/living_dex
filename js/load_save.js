91e4400ed7fac0d8e7bcc7276913d49186f6d5af
probably-not-porter <pelibby16@earlham.edu>
2022-02-05 20:20:50 -0500

    improve browsing

--- js/load_save.js
+++ js/load_save.js
@@ -1,9 +1,9 @@
 function read(){
-    console.log("READ");
     dex_progress = JSON.parse(localStorage.getItem("dex_progress")) || [];
-    console.log("save state: " + dex_progress)
+    console.info("Loaded Dex from local storage.");
 }
     
 function write(){
     localStorage.setItem("dex_progress", JSON.stringify(dex_progress));
+    console.info("Saved Dex to local storage.");
 }
--- js/main.js
+++ js/main.js
@@ -1,35 +1,35 @@
 boxes = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
 box_names = [
-    "Dex 001-030 (Box 01)", 
-    "Dex 031-060 (Box 02)", 
-    "Dex 061-090 (Box 03)", 
-    "Dex 091-120 (Box 04)", 
-    "Dex 121-150 (Box 05)", 
-    "Dex 151-180 (Box 06)", 
-    "Dex 181-210 (Box 07)", 
-    "Dex 211-240 (Box 08)", 
-    "Dex 241-270 (Box 09)", 
-    "Dex 271-300 (Box 10)",
-    "Dex 301-330 (Box 11)", 
-    "Dex 331-360 (Box 12)", 
-    "Dex 361-390 (Box 13)", 
-    "Dex 391-420 (Box 14)", 
-    "Dex 421-450 (Box 15)", 
-    "Dex 451-480 (Box 16)", 
-    "Dex 481-510 (Box 17)", 
-    "Dex 511-540 (Box 18)", 
-    "Dex 541-570 (Box 19)", 
-    "Dex 571-600 (Box 20)",
-    "Dex 601-630 (Box 21)", 
-    "Dex 631-660 (Box 22)", 
-    "Dex 661-690 (Box 23)", 
-    "Dex 691-720 (Box 24)", 
-    "Dex 721-750 (Box 25)", 
-    "Dex 751-780 (Box 26)", 
-    "Dex 781-810 (Box 27)", 
-    "Dex 811-840 (Box 28)", 
-    "Dex 841-870 (Box 29)", 
-    "Dex 871-898 (Box 30)",
+    "National Dex 001-030", 
+    "National Dex 031-060", 
+    "National Dex 061-090", 
+    "National Dex 091-120", 
+    "National Dex 121-150", 
+    "National Dex 151-180", 
+    "National Dex 181-210", 
+    "National Dex 211-240", 
+    "National Dex 241-270", 
+    "National Dex 271-300",
+    "National Dex 301-330", 
+    "National Dex 331-360", 
+    "National Dex 361-390", 
+    "National Dex 391-420", 
+    "National Dex 421-450", 
+    "National Dex 451-480", 
+    "National Dex 481-510", 
+    "National Dex 511-540", 
+    "National Dex 541-570", 
+    "National Dex 571-600",
+    "National Dex 601-630", 
+    "National Dex 631-660", 
+    "National Dex 661-690", 
+    "National Dex 691-720", 
+    "National Dex 721-750", 
+    "National Dex 751-780", 
+    "National Dex 781-810", 
+    "National Dex 811-840", 
+    "National Dex 841-870", 
+    "National Dex 871-898",
     "Empty (Box 31)", 
     "Empty (Box 32)"
 ]
@@ -48,19 +48,32 @@ function populate_living_dex(){
         boxes[target_box].push(key)
         count = count + 1;
     }
-    console.log(boxes)
 }
 
 function render_box(n){
+    console.warn("Render Page " + n);
+    // Update DEX TOTAL
+    document.getElementById("total").innerHTML = "Dex Completion: " + dex_progress.length + "/" + Object.keys(dex).length + " (" + ((dex_progress.length / Object.keys(dex).length) * 100).toFixed(2) + "%)";
+    // collect objects
     let data = boxes[n];
     let spaces = document.getElementsByClassName("entry");
     let navs = document.getElementsByClassName("box-nav");
+
+    // render navs (with count)
     for (x = 0;x < navs.length; x++){
         navs[x].classList.remove('active');
-        navs[x].innerHTML = box_names[x];
+        let count = 0;
+        for (y = 1+x*30; y < 31+x*30;y++){
+            if (dex_progress.includes(y)){
+                count += 1;
+            }
+        }        
+        navs[x].innerHTML = "<img src='img/great.png'> <span class='box-name'>" + box_names[x] + "</span><span class='box-count'>" + " (" + count + "/30)</span>";
     }
-    navs[n].classList.add('active');
-    document.getElementById("box-title").innerHTML = box_names[n];
+    navs[n].classList.add('active'); // one of them is active
+    document.getElementById("box-title").innerHTML = box_names[n]; // title current box
+
+    // Render box pkmn
     for (x = 0; x < 30; x++){
         if (x < data.length){
             let name = dex[data[x]];
@@ -77,18 +90,15 @@ function render_box(n){
             }else{
                 spaces[x].classList.remove('check');
             }
-            
         }
         else{
             spaces[x].innerHTML = "<div class='bg'></div>"
             spaces[x].style.backgroundImage = "";
-
         }
     }
        
 }
 function toggle_pkmn(n){
-    console.log(dex_progress);
     if (dex_progress.includes(n)){
         dex_progress = dex_progress.filter(function(item) {
             return item !== n
@@ -96,11 +106,13 @@ function toggle_pkmn(n){
     }else{
         dex_progress.push(n);
     }
-    console.log(dex_progress);
     write();
     render_box(box_current);
 }
-
+function goto_box(n){
+    box_current = n;
+    render_box(box_current);
+}
 function box_left(){
     box_current = box_current - 1 
     if (box_current == -1) {box_current = 31}
@@ -112,10 +124,9 @@ function box_right(){
     render_box(box_current);
 }
 
-
 window.addEventListener('load', (event) => {
-    console.log('page is fully loaded');
     read();
     populate_living_dex();
     render_box(box_current);
+    console.info('Page is fully loaded.');
 });
