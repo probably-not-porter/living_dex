da1a66b8e9e33821d9b36b4af7d8145f56ed1a6b
probably-not-porter <pelibby16@earlham.edu>
2022-02-06 14:46:48 -0500

    import export

--- js/load_save.js
+++ js/load_save.js
@@ -1,3 +1,5 @@
+// Read and Write information from local storage or file
+
 function read(){
     dex_progress = JSON.parse(localStorage.getItem("dex_progress")) || [];
     console.info("Loaded Dex from local storage.");
@@ -7,3 +9,27 @@ function write(){
     localStorage.setItem("dex_progress", JSON.stringify(dex_progress));
     console.info("Saved Dex to local storage.");
 }
+
+function write_file() {
+    const a = document.createElement('a');
+    const file = new Blob([dex_progress], {type: 'text/plain'});
+    a.href= URL.createObjectURL(file);
+    a.download = "living_dex_organizer.save";
+    a.click();
+    URL.revokeObjectURL(a.href);
+}
+
+window.addEventListener('load', (event) => {
+    document.getElementById("import_file").innerHTML = "Test";
+    document.getElementById('import_file').addEventListener('change', function() {
+        var fr=new FileReader();
+        fr.onload=function(){
+            let text = fr.result;
+            dex_progress = text.split(",").map(Number);
+            write();
+        }  
+        fr.readAsText(this.files[0]);
+        render_box(box_current);
+        alert("Save file loaded, stored in browser cache.")
+    });
+});
